4a822665779cb9eee262fe75760b38e5a3b70bf9
Shikanime Deva <deva.shikanime@protonmail.com>
2019-03-12 10:13:10 +0100

    fix: position mutation

--- src/event/game.c
+++ src/event/game.c
@@ -18,36 +18,36 @@ void        handle_game_inputs(SDL_Event *event, t_game *game)
                 case SDLK_UP:
                     y = compute_bomberman_up_move(game->map, game->player->y);
                     if (move_bomberman(game->map, game->player->x, game->player->y, game->player->x, y)) {
-                        game->player->y = y;
                         sprintf(res_buff, "mv up %d %d", game->player->x, game->player->y);
                         send_event(game->server, res_buff);
+                        game->player->y = y;
                     }
                     break;
 
                 case SDLK_DOWN:
                     y = compute_bomberman_down_move(game->map, game->player->y);
                     if (move_bomberman(game->map, game->player->x, game->player->y, game->player->x, y)) {
-                        game->player->y = y;
                         sprintf(res_buff, "mv down %d %d", game->player->x, game->player->y);
                         send_event(game->server, res_buff);
+                        game->player->y = y;
                     }
                     break;
 
                 case SDLK_LEFT:
                     x = compute_bomberman_left_move(game->map, game->player->x);
                     if (move_bomberman(game->map, game->player->x, game->player->y, x, game->player->y)) {
-                        game->player->x = x;
                         sprintf(res_buff, "mv left %d %d", game->player->x, game->player->y);
                         send_event(game->server, res_buff);
+                        game->player->x = x;
                     }
                     break;
 
                 case SDLK_RIGHT:
                     x = compute_bomberman_right_move(game->map, game->player->x);
                     if (move_bomberman(game->map, game->player->x, game->player->y, x, game->player->y)) {
-                        game->player->x = x;
                         sprintf(res_buff, "mv right %d %d", game->player->x, game->player->y);
                         send_event(game->server, res_buff);
+                        game->player->x = x;
                     }
                     break;
 
@@ -77,13 +77,13 @@ void        handle_server_events(t_game *game, char *packet)
         if (place_bomberman(game->map, create_bomberman(), x, y)) {
             char discorvery_packet[20];
 
-            printf("A new player have been connected at %d:%d", x, y);
+            printf("A new player have been connected at %d:%d\n", x, y);
             sprintf(discorvery_packet, "spawn %02d %02d", game->player->x, game->player->y);
             send_event(game->server, discorvery_packet);
         }
-    } else if (sscanf(packet, "mv down %02d %02d", &x, &y) == 2) {
-        move_bomberman(game->map, x, y, x, compute_bomberman_up_move(game->map, y));
     } else if (sscanf(packet, "mv up %02d %02d", &x, &y) == 2) {
+        move_bomberman(game->map, x, y, x, compute_bomberman_up_move(game->map, y));
+    } else if (sscanf(packet, "mv down %02d %02d", &x, &y) == 2) {
         move_bomberman(game->map, x, y, x, compute_bomberman_down_move(game->map, y));
     } else if (sscanf(packet, "mv left %02d %02d", &x, &y) == 2) {
         move_bomberman(game->map, x, y, compute_bomberman_left_move(game->map, x), y);
